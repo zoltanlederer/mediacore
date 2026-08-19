fa17210be2551ab1f69f90e89b9aa76b2b30a23e
Zoltan Lederer <23390588+zoltanlederer@users.noreply.github.com>
2026-08-19 21:34:14 +0200

    Fetch and display raw media data from GET /media

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,19 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState<any[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/media')
+    .then(res => res.json())
+    .then(fetchedData => {
+      setData(fetchedData)
+    })
+  }, [])
 
   return (
     <>
+    <pre>{JSON.stringify(data)}</pre>
     </>
   )
 }
