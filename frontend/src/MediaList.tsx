aaaadf8724852fc6877287ffd31bb5693e768b16
Zoltan Lederer <23390588+zoltanlederer@users.noreply.github.com>
2026-08-30 00:22:03 +0200

    Add responsive styles for mobile: stacked header, scrollable table, stacked detail view

--- frontend/src/MediaList.tsx
+++ frontend/src/MediaList.tsx
@@ -10,48 +10,48 @@ function MediaList() {
 	}
 
 	return (
-		<>
-		<table className="media-table">
-		<thead>
-				<tr>
-				<th colSpan={2}>TITLE</th>
-				<th>GENRE</th>
-				<th className="col-year">YEAR</th>
-				<th className="col-rating">RATING</th>
-				<th>TYPE</th>
-				<th className="col-watched">WATCHED</th>
+		<div className="media-table-wrapper">
+			<table className="media-table">
+			<thead>
+					<tr>
+					<th colSpan={2}>TITLE</th>
+					<th>GENRE</th>
+					<th className="col-year">YEAR</th>
+					<th className="col-rating">RATING</th>
+					<th>TYPE</th>
+					<th className="col-watched">WATCHED</th>
+					</tr>
+			</thead>
+			<tbody>
+				{context.data.map(item => (
+				<tr key={item.index}>
+					<td>
+						<img
+							className="poster-thumbnail"
+							src={item.poster_path ? `https://image.tmdb.org/t/p/w92${item.poster_path}` : "/images/poster-placeholder.png"}
+							alt={item.title}
+						/>
+					</td>
+					<td>
+						<Link to={`/media/${item.index}`}>{item.title}</Link>
+					</td>
+					<td>{item.genres}</td>
+					<td className="col-year">{item.year}</td>
+					<td className="col-rating">{item.imdb_rating}</td>
+					<td>{item.type}</td>
+					<td className="col-watched">
+						<span
+							className={`watched-toggle ${item.watched ? 'watched' : 'unwatched'}`}
+							onClick={() => context.onWatchedToggle(item.index)}
+						>
+							{item.watched ? '✓' : '✕'}
+						</span>
+					</td>
 				</tr>
-		</thead>
-		<tbody>
-			{context.data.map(item => (
-			<tr key={item.index}>
-				<td>
-					<img
-						className="poster-thumbnail"
-						src={item.poster_path ? `https://image.tmdb.org/t/p/w92${item.poster_path}` : "/images/poster-placeholder.png"}
-						alt={item.title}
-					/>
-				</td>
-				<td>
-					<Link to={`/media/${item.index}`}>{item.title}</Link>
-				</td>
-				<td>{item.genres}</td>
-				<td className="col-year">{item.year}</td>
-				<td className="col-rating">{item.imdb_rating}</td>
-				<td>{item.type}</td>
-				<td className="col-watched">
-					<span
-						className={`watched-toggle ${item.watched ? 'watched' : 'unwatched'}`}
-						onClick={() => context.onWatchedToggle(item.index)}
-					>
-						{item.watched ? '✓' : '✕'}
-					</span>
-				</td>
-			</tr>
-			))}
-		</tbody>
-		</table>
-		</>
+				))}
+			</tbody>
+			</table>
+		</div>
 	)
 }
 
