f24d1183fa1a2f80e1d633065642196de889a744
Zoltan Lederer <23390588+zoltanlederer@users.noreply.github.com>
2026-08-30 21:48:01 +0200

    Use PORT environment variable for deployment compatibility

--- api/src/index.ts
+++ api/src/index.ts
@@ -170,6 +170,7 @@ app.patch('/media/:index/watched', (req: Request<{index: number}, {}, WatchedUpd
     }
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 });
